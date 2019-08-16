275bf489eb5500c309efac95ce379d98e9c2bb70
NightOWL00 <47278518+NightOWL00@users.noreply.github.com>
2019-08-17 02:34:35 +0530

    Update gui.py

--- gui.py
+++ gui.py
@@ -8,7 +8,7 @@ password = pg.password()
 def clicked_gen():
     # passwordStr
     passwordStr = t.Label(window, text=str(password), font=("Comic Sans", 20))
-    passwordStr.grid(column=0, row=2, padx=(10, 10), pady=(5, 5))
+    passwordStr.grid(column=1, row=1, padx=(10, 10), pady=(5, 5))
 
     # copy Button
     copyButton = t.Button(window, text='Copy', command=copyButtonClicked)
@@ -30,12 +30,13 @@ window.title("Password Generator and store")
 # Title Label
 titleLabel = t.Label(window, text="Click Generate to get a password",
                      font=("Comic Sans", 15))
-titleLabel.grid(column=0, row=0, padx=(10, 10), pady=(5, 5))
+titleLabel.grid(column=0, row=0, padx=(45, 45), pady=(5, 5), columnspan=2)
 
 # gen button
 GenerateButton = t.Button(window, text='Generate', bg="springgreen2",
                           fg="black", command=clicked_gen)
-GenerateButton.grid(column=0, row=1, padx=(10, 10), pady=(5, 5))
+GenerateButton.grid(column=0, row=1, padx=(10, 0), pady=(
+    5, 5), ipadx=(5), ipady=(2), sticky='W')
 
 
 # Exit Button
