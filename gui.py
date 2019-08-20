4da1e162be17a72e0dba42b2926d33333c44d297
Kushagra Gangwar <kushagragangwar4@gmail.com>
2019-08-20 20:20:11 +0530

    Update gui.py

--- gui.py
+++ gui.py
@@ -7,22 +7,22 @@ password = pg.password()
 
 def clicked_gen():
     # passwordStr
-    passwordStr = t.Label(window, text=str(password), font=("Comic Sans", 20))
+    passwordStr = t.Label(window1, text=str(password), font=("Comic Sans", 20))
     passwordStr.grid(column=0, row=1, padx=(20, 25), pady=(5, 5), sticky='E')
 
     # copy Button
-    copyButton = t.Button(window, text='Copy', command=copyButtonClicked, bg="springgreen2",
+    copyButton = t.Button(window1, text='Copy', command=copyButtonClicked, bg="springgreen2",
                           fg="black",)
     copyButton.grid(column=0, row=4, padx=(10, 10), pady=(
         20, 10), ipadx=(5), ipady=(2), sticky='W')
     # Password Strength
-    passwordStrength = t.Label(window, text='Password Strength (Between 0 and 1) is '+str(pg.passStrength(password))[:7]+')', bg="springgreen2",
+    passwordStrength = t.Label(window1, text='Password Strength (Between 0 and 1) is '+str(pg.passStrength(password))[:7]+')', bg="springgreen2",
                                fg="black")
     passwordStrength.grid(column=0, row=3, padx=(10, 30),
                           pady=(5, 5), sticky='E')
 
     # save
-    saveButton = t.Button(window, text='Save Password',
+    saveButton = t.Button(window1, text='Save Password',
                           bg="springgreen2", fg="black")
     saveButton.grid(column=0, row=5, padx=(10, 0), pady=(
         5, 5), ipadx=(5), ipady=(2), sticky='W')
@@ -31,34 +31,46 @@ def clicked_gen():
 def copyButtonClicked():
     pc.copy(password)
 
+def createwindow():
 
-window = t.Tk()
-window.geometry('400x300')
-window.title("Password Generator and store")
-window.resizable(0, 0)
+    #Window of UserID
+    window2 = t.Tk()
+##    window1.geometry('400x300')
+##    window1.title('UserID')
+    canvas1 = t.Canvas(window2, width = 400, height = 300)
+    canvas1.pack()
+
+    entry1 = t.Entry()
+    canvas1.create_window(200, 140, window=entry1)
+    
+
+window1 = t.Tk()
+window1.geometry('400x300')
+window1.title("Password Generator and store")
+window1.resizable(0, 0)
 
 # Title Label
-titleLabel = t.Label(window, text="Click Generate to get a password",
+titleLabel = t.Label(window1, text="Click Generate to get a password",
                      font=("Comic Sans", 15))
 titleLabel.grid(column=0, row=0, padx=(45, 45), pady=(5, 5))
 
 # gen button
-GenerateButton = t.Button(window, text='Generate', bg="springgreen2",
+GenerateButton = t.Button(window1, text='Generate', bg="springgreen2",
                           fg="black", command=clicked_gen)
 GenerateButton.grid(column=0, row=1, padx=(10, 0), pady=(
     5, 5), ipadx=(5), ipady=(2), sticky='W')
 
 # view passwords
-viewButton = t.Button(window, text='View saved passwords',
-                      bg="springgreen2", fg="black")
+viewButton = t.Button(window1, text='View saved passwords',
+                      bg="springgreen2", fg="black",command=createwindow)
 viewButton.grid(column=0, row=5, padx=(10, 0), pady=(
     5, 5), ipadx=(5), ipady=(2), sticky='E')
 
 # Exit Button
-exit_button = t.Button(window, text='EXIT', bg='black',
-                       fg='white', command=window.destroy)
+exit_button = t.Button(window1, text='EXIT', bg='black',
+                       fg='white', command=window1.destroy)
 exit_button.grid(column=0, row=4, padx=(10, 10),
                  pady=(20, 10), ipadx=(5), ipady=(2), sticky='E')
 
 
-window.mainloop()
+window1.mainloop()
