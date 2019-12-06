25fdf13baf5622f1bf22319a42264f32723628b3
NightOWL00 <47278518+NightOWL00@users.noreply.github.com>
2019-12-06 23:13:41 +0530

    bugs fixed

--- main_file.py
+++ main_file.py
@@ -5,7 +5,10 @@ import random
 import string
 import pyperclip as pc
 import os
-os.mkdir('DATA_FILES')
+if os.path.exists('DATA_FILES'):
+    pass
+else:
+    os.mkdir('DATA_FILES')
 os.system("attrib +h DATA_FILES")
 if os.path.exists('DATA_FILES/data_pgs.dtap'):
     os.rename('DATA_FILES/data_pgs.dtap',
@@ -46,6 +49,18 @@ def password():
     return x
 
 
+def view_passwords():
+    file = 'DATA_FILES\Temp_file_DELETE_AFTER_USE.txt'  # windows file system
+    Message = 'Delete this file after use to avoid password leakage'
+    f = open(file, 'w')
+    f.write(Message+'\n')
+    with open("DATA_FILES/Good_File_DO_NOT_DELETE_Contains_password.txt") as password_file:
+        for line in password_file.readlines():
+            f.write(decrypt(line).decode() + '\n')
+    f.close()
+    os.popen(file)
+
+
 window = Tk()
 window.title('Password generator')
 
@@ -80,17 +95,6 @@ def magic():
         f.write('\n' + encrypt(data).decode() + '\n')
         f.close()
 
-    def view_passwords():
-        file = 'DATA_FILES\Temp_file_DELETE_AFTER_USE.txt'  # windows file system
-        Message = 'Delete this file after use to avoid password leakage'
-        f = open(file, 'w')
-        f.write(Message+'\n')
-        with open("DATA_FILES/Good_File_DO_NOT_DELETE_Contains_password.txt") as password_file:
-            for line in password_file.readlines():
-                f.write(decrypt(line).decode() + '\n')
-        f.close()
-        os.popen(file)
-
     ent1 = Entry(window, width=20)
     ent1.grid(column=1, row=4, padx=(10, 0), pady=(5, 5), ipadx=(5), ipady=(2))
 
@@ -98,16 +102,20 @@ def magic():
                   fg="springgreen2", command=save_it)
     btn2.grid(column=0, row=5, padx=(10, 0), pady=(
         5, 5), ipadx=(5), ipady=(2))
-    btn3 = Button(window, text='    View Passwords    ', bg="black",
-                  fg="springgreen2", command=view_passwords)
     btn3.grid(column=1, row=5, padx=(10, 0), pady=(
         5, 5), ipadx=(5), ipady=(2))
+    btn1.grid(column=0, row=0, padx=(10, 0), pady=(
+        5, 5), ipadx=(5), ipady=(2), columnspan=2)
 
 
 btn1 = Button(window, text='Generate and copy',
               bg="black", fg="springgreen2", command=magic)
 btn1.grid(column=0, row=0, padx=(10, 0), pady=(
-    5, 5), ipadx=(5), ipady=(2), columnspan=2)
+    5, 5), ipadx=(5), ipady=(2), columnspan=1)
+
+btn3 = Button(window, text='    View Passwords    ', bg="black",
+              fg="springgreen2", command=view_passwords)
+btn3.grid(column=1, row=0, padx=(10, 0), pady=(5, 5), ipadx=(5), ipady=(2))
 
 lbl1 = Label(window, text=' ')
 lbl1.grid(column=0, row=1, padx=(10, 0), pady=(
